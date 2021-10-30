0330ca5e5f8c2e0d2bfa7df0e306d8581a8812c5
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-29 22:49:55 -0500

    Added edit and delete paths and templates

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -30,3 +30,30 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    hero = Superhero.objects.get(pk=hero_id)
+    context =  {
+        'hero': hero
+    }
+    if request.method == 'POST':
+        hero.name = request.POST.get('name')
+        hero.alter_ego = request.POST.get('alter_ego')
+        hero.primary_ability = request.POST.get('primary_ability')
+        hero.secondary_ability = request.POST.get('secondary_ability')
+        hero.catch_phrase = request.POST.get('catch_phrase')
+        hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/edit.html', context)
+
+def delete(request, hero_id):
+    hero = Superhero.objects.get(pk=hero_id)
+    context =  {
+        'hero': hero
+    }
+    if request.method == 'POST':
+        hero.delete()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/delete.html', context)
